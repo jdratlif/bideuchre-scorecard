65aa45043ceb9cfa38f6b8385545e362f3511e29
John Ratliff <john@technoplaza.net>
2022-07-23 18:07:27 -0400

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div>Bid Euchre Score Card</div>;
+  return (
+    <div>
+      <h1 className="text-3xl text-center">Bid Euchre Score Card</h1>
+    </div>
+  );
 };
 
 export default App;
